448954d71e9098e544bc1975fd66181ff517cd1a
iCrazyBlaze <iCrazyBlaze@users.noreply.github.com>
2021-12-04 14:39:17 +0000

    More unused code cleanup

--- src/main/java/io/github/icrazyblaze/beefyupdate/util/ForgeEventSubscribers.java
+++ src/main/java/io/github/icrazyblaze/beefyupdate/util/ForgeEventSubscribers.java
@@ -1,8 +1,6 @@
 package io.github.icrazyblaze.beefyupdate.util;
 
 import io.github.icrazyblaze.beefyupdate.Main;
-import io.github.icrazyblaze.beefyupdate.entity.BeefGolemEntity;
-import io.github.icrazyblaze.beefyupdate.entity.BeefGolemRenderer;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,15 +14,5 @@ public class ForgeEventSubscribers {
 
     public static final DeferredRegister<GlobalLootModifierSerializer<?>> GLM = DeferredRegister.create(ForgeRegistries.LOOT_MODIFIER_SERIALIZERS, Main.MOD_ID);
     public static final RegistryObject<CowLootModifier.Serializer> COW_ANVIL_LOOT_MODIFIER = GLM.register("cow", CowLootModifier.Serializer::new);
-
-    @SubscribeEvent
-    public static void registerEntities(EntityAttributeCreationEvent event) {
-        event.put(Main.BEEF_GOLEM, BeefGolemEntity.createAttributes().build());
-    }
-    
-    @SubscribeEvent
-    public static void registerEntityRenderers(FMLClientSetupEvent event) {
-        RenderingRegistry.registerEntityRenderingHandler(Main.BEEF_GOLEM, BeefGolemRenderer::new);
-    }
 
 }
