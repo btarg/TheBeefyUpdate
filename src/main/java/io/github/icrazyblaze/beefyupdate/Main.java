71494609116c394387f07c0e5b95b0693bcc5cfa
iCrazyBlaze <icrazyblaze1@gmail.com>
2022-02-06 16:31:48 +0000

    Remove unused code

--- src/main/java/io/github/icrazyblaze/beefyupdate/Main.java
+++ src/main/java/io/github/icrazyblaze/beefyupdate/Main.java
@@ -7,9 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.SpawnEggItem;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -52,13 +50,6 @@ public class Main {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.register(ForgeEventSubscribers.class);
         ModItems.ITEMS.register(bus);
-
-        // Here we need to register the Golem entity before the Spawn Egg item to prevent bugs
-        ENTITIES.register(bus);
-        ENTITIES.register("beef_golem", () -> BEEF_GOLEM);
-
-        ModItems.ITEMS.register("beef_golem_spawn_egg", () ->
-                new SpawnEggItem(BEEF_GOLEM, 0x5d3829, 0x30180f, new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
         // Global loot modifiers
         ForgeEventSubscribers.GLM.register(bus);
